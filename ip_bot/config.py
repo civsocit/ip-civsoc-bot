3b7328f46d31e5591891d1eefc6db3223dd489f3
DavisDmitry <carlversalski@gmail.com>
2020-12-21 19:12:12 +0500

    add optional redis fsm

--- ip_bot/config.py
+++ ip_bot/config.py
@@ -3,12 +3,18 @@ from typing import Optional, Union
 
 
 class Config:
-    _fields = ['TOKEN', 'LOG_LEVEL', 'DIRECTORS_CHAT', 'REDACTION_CHAT']
+    _fields = ['TOKEN', 'DIRECTORS_CHAT', 'REDACTION_CHAT',
+               'REDIS_HOST', 'REDIS_PORT', 'REDIS_INDEX', 'REDIS_PASS',
+               'LOG_LEVEL']
 
     TOKEN: str
-    LOG_LEVEL: str = 'DEBUG'
     DIRECTORS_CHAT: int
     REDATCION_CHAT: int
+    REDIS_HOST: Optional[str]
+    REDIS_PORT: Optional[int]
+    REDIS_INDEX: int = 0
+    REDIS_PASS: Optional[str]
+    LOG_LEVEL: str = 'DEBUG'
 
     @classmethod
     def from_env(cls):
@@ -21,10 +27,18 @@ class Config:
                  token: str,
                  directors_chat: Union[int, str],
                  redaction_chat: Union[int, str],
+                 redis_host: Optional[str] = None,
+                 redis_port: Optional[int] = None,
+                 redis_index: int = 0,
+                 redis_pass: Optional[str] = None,
                  log_level: Optional[str] = None):
         self.TOKEN = token
         self.DIRECTORS_CHAT = int(directors_chat)
         self.REDACTION_CHAT = int(redaction_chat)
+        self.REDIS_HOST = redis_host
+        self.REDIS_PORT = redis_port
+        self.REDIS_INDEX = redis_index
+        self.REDIS_PASS = redis_pass
         if log_level:
             self.LOG_LEVEL = self._validate_log_level(log_level)
 
--- ip_bot/main.py
+++ ip_bot/main.py
@@ -2,6 +2,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from ip_bot.config import Config
 from ip_bot.filters import register_filters
@@ -11,7 +12,14 @@ from ip_bot.handlers import register_handlers
 config = Config.from_env()
 bot = Bot(config.TOKEN, parse_mode='html')
 bot.config = config
-dp = Dispatcher(bot, storage=MemoryStorage())
+if config.REDIS_HOST and config.REDIS_PORT:
+    storage = RedisStorage2(host=config.REDIS_HOST,
+                            port=config.REDIS_PORT,
+                            db=config.REDIS_INDEX,
+                            password=config.REDIS_PASS)
+else:
+    storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 
 logging.basicConfig(level=config.LOG_LEVEL)
